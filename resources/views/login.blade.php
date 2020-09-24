9e73e6db6b7e17f7c6631a536a41e4380c60fdde
Gustavo Pires Trentini <gustavopires1903@gmail.com>
2020-09-24 11:21:16 -0300

    Correção de tela de login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,40 +1,40 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <title>Signin </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/sign-in/">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('css/singin.css')}}">
+    <!-- <link rel="stylesheet" href="{{asset('css/singin.css')}}"> -->
 
 </head>
 
-<body class="text-center">
-    <form class="form-signin">
-        <div class="container-fluid shadow-sm p-3 mb-5 bg-dark text-light rounded">
+<body>
+    <div class="container-fluid d-flex align-items-center justify-content-center" style="height: 100vh;background: #222;">
+        <form class="form-signin text-center col-md-3">
+            <div class="shadow-sm p-3 mb-5 bg-dark text-light rounded">
 
-        <div class="mb-4">
-            <img  src="{{asset('/images/logoKraken.png')}}" alt="" width="72" height="72">
-        </div>
+                <div class="mb-4">
+                    <img src="{{asset('/images/logoKraken.png')}}" alt="" width="72" height="72">
+                </div>
 
 
-        <h1 class="h2 mb-3 font-weight-normal">Realize o login</h1>
+                <h1 class="h2 mb-3 font-weight-normal">Realize o login</h1>
 
-        <label for="inputEmail" class="sr-only">Email:</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                <input type="email" id="inputEmail" class="form-control mb-2" placeholder="Email" required autofocus>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
 
-        <div class="checkbox mb-3">
-            <label>
-                <input type="checkbox" value="remember-me"> Lembrar senha
-            </label>
-        </div>
+                <div class="checkbox mb-3">
+                    <label>
+                        <input type="checkbox" value="remember-me"> Lembrar senha
+                    </label>
+                </div>
 
-        <button class="btn btn-lg btn-outline-light btn-block" type="submit">Sign in</button>
-        <p class="mt-5 mb-3 text-muted">&copy; 2020 V1</p>
-        </div>
-    </form>
+                <button class="btn btn-lg btn-outline-light btn-block" type="submit">Sign in</button>
+                <p class="mt-5 mb-3 text-muted">&copy; 2020 V1</p>
+        </form>
+    </div>
 </body>
 
 </html>
